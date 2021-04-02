803266a9adfd76e5357b396bc8588af93686fd35
Daniel Rivas <daniel.rivas@hey.com>
2021-04-02 22:54:53 +0100

    Adding a single file works

--- src/index.rs
+++ src/index.rs
@@ -19,43 +19,43 @@ pub struct Index {
 }
 
 const MAX_PATH_SIZE: u16 = 0xfff;
-const REGULAR_MODE: u16 = 0o100644;
-const EXECUTABLE_MODE: u16 = 0o100755;
+const REGULAR_MODE: u32 = 0o100644;
+const EXECUTABLE_MODE: u32 = 0o100755;
 
 pub struct Entry {
-    ctime: i64,
-    ctime_nsec: i64,
-    mtime: i64,
-    mtime_nsec: i64,
-    dev: u64,
-    ino: u64,
-    mode: u16,
+    ctime: u32,
+    ctime_nsec: u32,
+    mtime: u32,
+    mtime_nsec: u32,
+    dev: u32,
+    ino: u32,
+    mode: u32,
     uid: u32,
     gid: u32,
-    size: u64,
+    size: u32,
     oid: ObjectId,
-    flags: usize,
+    flags: u16,
     path: OsString,
 }
 
 impl Entry {
     pub fn new(path: &OsStr, oid: ObjectId, stat: Metadata) -> Self {
-        let ctime = stat.ctime();
-        let ctime_nsec = stat.ctime_nsec();
-        let mtime = stat.mtime();
-        let mtime_nsec = stat.mtime_nsec();
-        let dev = stat.dev();
-        let ino = stat.ino();
-        let uid = stat.uid();
-        let gid = stat.gid();
-        let size = stat.size();
+        let ctime = stat.ctime() as u32;
+        let ctime_nsec = stat.ctime_nsec() as u32;
+        let mtime = stat.mtime() as u32;
+        let mtime_nsec = stat.mtime_nsec() as u32;
+        let dev = stat.dev() as u32;
+        let ino = stat.ino() as u32;
+        let uid = stat.uid() as u32;
+        let gid = stat.gid() as u32;
+        let size = stat.size() as u32;
         let mode = if is_executable(stat.mode()) {
             EXECUTABLE_MODE
         } else {
             REGULAR_MODE
         };
 
-        let flags = usize::min(path.as_bytes().len(), MAX_PATH_SIZE as usize);
+        let flags = u16::min(path.as_bytes().len() as u16, MAX_PATH_SIZE);
 
         let path = path.to_owned();
 
@@ -65,19 +65,54 @@ impl Entry {
             mtime,
             mtime_nsec,
             dev,
+            ino,
             mode,
-            path,
             uid,
             gid,
+            size,
             oid,
             flags,
-            size,
-            ino,
+            path,
         }
     }
 
     pub fn bytes(&self) -> Vec<u8> {
-        let bytes = Vec::new();
+        const ENTRY_BLOCK: usize = 8;
+
+        let mut bytes = Vec::new();
+
+        let Self {
+            ctime,
+            ctime_nsec,
+            mtime,
+            mtime_nsec,
+            dev,
+            ino,
+            mode,
+            uid,
+            gid,
+            size,
+            oid,
+            flags,
+            path,
+        } = &self;
+
+        for &item in &[
+            ctime, ctime_nsec, mtime, mtime_nsec, dev, ino, mode, uid, gid, size,
+        ] {
+            let bs = item.to_be_bytes();
+            bytes.extend_from_slice(&bs);
+        }
+
+        bytes.extend_from_slice(oid.bytes());
+        bytes.extend_from_slice(&flags.to_be_bytes());
+        bytes.extend_from_slice(path.as_bytes());
+        bytes.extend_from_slice(b"\0");
+
+        while bytes.len() % ENTRY_BLOCK != 0 {
+            bytes.push(b'\0');
+        }
+
         bytes
     }
 }
