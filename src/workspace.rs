1c8ff082b4284434b62a882b5afaeadcb7686765
Daniel Rivas <daniel.rivas@hey.com>
2021-04-03 14:53:46 +0100

    Allow adding directories

--- src/workspace.rs
+++ src/workspace.rs
@@ -17,51 +17,52 @@ impl Workspace {
         }
     }
 
-    pub fn list_files(&self) -> anyhow::Result<Vec<OsString>> {
-        self._list_files(None)
+    fn _list_files(&self, path: Option<&Path>) -> anyhow::Result<Vec<OsString>> {
+        let path = path.unwrap_or(&self.pathname);
+
+        let res = if std::fs::metadata(path)?.is_dir() {
+            let dirs = std::fs::read_dir(path)?;
+            let mut file_names = Vec::new();
+            for dir in dirs {
+                let path = dir?.path();
+                if !&[".", "..", ".git"].iter().any(|&s| path.ends_with(s)) {
+                    let file_name = path
+                        .file_name()
+                        .ok_or_else(|| anyhow!("Couldn't get path filename"))?
+                        .to_owned();
+
+                    file_names.push(file_name);
+                }
+            }
+            file_names
+                .iter()
+                .map(|name| self._list_files(Some(&path.join(name))))
+                .flat_map(|result| match result {
+                    Ok(vec) => vec.into_iter().map(Ok).collect(),
+                    Err(e) => vec![Err(e)],
+                })
+                .collect()
+        } else {
+            Ok(vec![crate::utils::diff_paths(path, &self.pathname)
+                .ok_or_else(|| anyhow!("Couldn't get relative path"))?
+                .as_os_str()
+                .to_owned()])
+        };
+
+        res
     }
 
-    fn _list_files(&self, dir: Option<&Path>) -> anyhow::Result<Vec<OsString>> {
-        let dir = dir.unwrap_or(&self.pathname);
+    /// Lists all files in a path, relative to this workspace's base directory.
+    pub fn list_files<P>(&self, path: P) -> anyhow::Result<Vec<OsString>>
+    where
+        P: AsRef<Path>,
+    {
+        self._list_files(Some(path.as_ref()))
+    }
 
-        let dirs = std::fs::read_dir(dir)?;
-        let mut file_names = Vec::new();
-        for dir in dirs {
-            let path = dir?.path();
-            if !&[".", "..", ".git"].iter().any(|&s| path.ends_with(s)) {
-                let file_name = path
-                    .file_name()
-                    .ok_or_else(|| anyhow!("Couldn't get path filename"))?
-                    .to_owned();
-
-                file_names.push(file_name);
-            }
-        }
-
-        let file_names = file_names
-            .iter()
-            .map(|name| -> anyhow::Result<Vec<OsString>> {
-                let path = dir.join(name);
-
-                if path.is_dir() {
-                    // Recurse into directory
-                    self._list_files(Some(&path))
-                } else {
-                    // It's a file, add it to the list
-                    Ok(vec![crate::utils::diff_paths(path, &self.pathname)
-                        .ok_or_else(|| anyhow!("Couldn't get relative path"))?
-                        .as_os_str()
-                        .to_owned()])
-                }
-            })
-            .flat_map(|result| match result {
-                Ok(vec) => vec.into_iter().map(Ok).collect(),
-                Err(e) => vec![Err(e)],
-            })
-            .collect();
-
-        println!("{:?}", file_names);
-        file_names
+    /// Lists all files in a workspace's base directory.
+    pub fn list_files_in_root(&self) -> anyhow::Result<Vec<OsString>> {
+        self._list_files(None)
     }
 
     pub fn read_file<P: AsRef<Path>>(&self, path: P) -> io::Result<Vec<u8>> {
@@ -81,7 +82,7 @@ mod test {
     fn list_files() {
         let ws = Workspace::new("../testnit");
 
-        let entries = ws.list_files().unwrap();
+        let entries = ws.list_files_in_root().unwrap();
 
         assert_eq!(
             entries,
