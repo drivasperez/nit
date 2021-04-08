043c54f42430ddfb15b2a12e917b791612d8f9f1
Daniel Rivas <daniel.rivas@hey.com>
2021-04-08 21:52:45 +0100

    Add main error type

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use thiserror::Error;
 pub mod database;
 pub mod index;
 pub mod lockfile;
@@ -5,3 +6,17 @@ pub mod refs;
 pub mod workspace;
 
 mod utils;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Workspace error")]
+    Workspace(#[from] workspace::WorkspaceError),
+    #[error("Index error")]
+    Index(#[from] index::IndexError),
+    #[error("Lockfile error")]
+    Lockfile(#[from] lockfile::LockfileError),
+    #[error("Database error")]
+    Database(#[from] database::DatabaseError),
+    #[error("Ref error")]
+    Ref(#[from] refs::RefError),
+}
